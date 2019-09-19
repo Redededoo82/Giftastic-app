5000d80a41f3b6b165e1d8740df0a10ed630a4af
AARON M <asmarcks@gmail.com>
2019-09-18 22:57:53 -0500

    ajax request operational with button click

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -1,1 +1,24 @@
+
+
+
+
+//variable for the topics
 var topics = ["skateboarding", "fire eating", "spiders", "praying mantis", "Gillian Anderson", "Battlefield 4", "taekwondo", "Drums"];
+
+
+
+
+$('button').on('click', function () {
+    var x = $(this).data("search");
+    // console.log(x);
+
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=kfsNu9OGKoolNogRfeogBHYhr3OBTtRa";
+    console.log(queryURL);
+
+    $.ajax({ url: queryURL, method: 'GET' })
+        .done(function (response) {
+
+            // console.log(response);
+            
+        })
+})
