87746f3a491dc23723e13e435f0462f9c1418497
AARON M <asmarcks@gmail.com>
2019-09-19 00:02:06 -0500

    modified get request to include gifs when button is clicked

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -12,13 +12,20 @@ $('button').on('click', function () {
     var x = $(this).data("search");
     // console.log(x);
 
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=kfsNu9OGKoolNogRfeogBHYhr3OBTtRa";
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=kfsNu9OGKoolNogRfeogBHYhr3OBTtRa&limit3";
     console.log(queryURL);
 
     $.ajax({ url: queryURL, method: 'GET' })
         .done(function (response) {
+            console.log(response);
+            for(var i=0;i<response.data.length;i++){
+                
+                $('#gifs').prepend("<img src='" + response.data[i].images.downsized.url + "'>");
 
-            // console.log(response);
-            
-        })
-})
+            }
+
+            })
+    })
+
+
+
