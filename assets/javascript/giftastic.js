7a8de6495206af6de20c5c97855f2ad878af7791
AARON M <asmarcks@gmail.com>
2019-09-19 22:20:00 -0500

    final push needs work still zzzz

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -3,29 +3,38 @@
 
 
 //variable for the topics
-var topics = ["skateboarding", "fire eating", "spiders", "praying mantis", "Gillian Anderson", "Battlefield 4", "taekwondo", "Drums"];
+var topics = ["skateboarding", "juggling", "spiders", "praying mantis", "Gillian Anderson", "Battlefield 4", "taekwondo", "Drums"];
 
 
 
 
-$('button').on('click', function () {
+$('button').on('click', function mammalickabooboo () {
     var x = $(this).data("search");
     // console.log(x);
-
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=kfsNu9OGKoolNogRfeogBHYhr3OBTtRa&limit3";
-    console.log(queryURL);
+    // console.dir(this);
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=kfsNu9OGKoolNogRfeogBHYhr3OBTtRa&limit=3";
+    // console.log(queryURL);
 
     $.ajax({ url: queryURL, method: 'GET' })
         .done(function (response) {
-            console.log(response);
-            for(var i=0;i<response.data.length;i++){
-                
+            // console.log(response);
+            for (var i = 0; i < response.data.length; i++) {
+
                 $('#gifs').prepend("<img src='" + response.data[i].images.downsized.url + "'>");
 
             }
 
-            })
-    })
+        })
+})
 
+    // $('#add-topic').on('click', function(){
+    //     event.preventDefault();
+    //     var topic = $("#topic-input").val().trim();
+    //     topics.push(topic);
+
+        
+    // });
+
+       
 
 
