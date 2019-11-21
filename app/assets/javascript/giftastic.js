e04de496ed9ea49c358ed64ac4a76c030147ec40
AARON M <asmarcks@gmail.com>
2019-11-21 14:49:14 -0600

    still working on heroku deploy

---
+++ app/assets/javascript/giftastic.js
@@ -0,0 +1,40 @@
+
+
+
+
+// //variable for the topics
+// var topics = ["skateboarding", "freestyle walking", "spiders", "praying mantis", "Gillian Anderson", "Battlefield 4", "taekwondo", "Drums"];
+
+
+
+
+// $('button').on('click', function mammalickabooboo () {
+//     var x = $(this).data("search");
+//     // console.log(x);
+//     // console.dir(this);
+//     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=kfsNu9OGKoolNogRfeogBHYhr3OBTtRa&limit=3";
+//     // console.log(queryURL);
+
+//     $.ajax({ url: queryURL, method: 'GET' })
+//         .done(function (response) {
+//             // console.log(response);
+//             for (var i = 0; i < response.data.length; i++) {
+
+//                 $('#gifs').prepend("<img src='" + response.data[i].images.downsized.url + "'>");
+
+//             }
+
+//         })
+// })
+
+//     // $('#add-topic').on('click', function(){
+//     //     event.preventDefault();
+//     //     var topic = $("#topic-input").val().trim();
+//     //     topics.push(topic);
+
+        
+//     // });
+
+       
+
+
